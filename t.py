e98e505511f012af09116b7be48d8f9b6aba08aa
song0910song <1409024971@qq.com>
2026-03-11 21:24:13 +0800

    面向云代理会话的 VS Code 中的检查点

--- t.py
+++ t.py
@@ -1,2 +1,18 @@
-i = 0b0011010
-print(bin(i << 3))
+from typing import List
+from math import inf
+from collections import defaultdict
+from bisect import bisect_right, bisect_left
+
+class Solution:
+    def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
+        p = sorted(potions)
+        n = len(p)
+        print(p)
+
+        return [n - bisect_left(p, success // si + 1) for si in spells]
+
+
+s = Solution()
+print(s.successfulPairs([3,1,2], [8, 5, 8], 16))
+import math
+print(math.ceil(16 / 3))
